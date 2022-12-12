7b63105cd5f014e3418e0c7ee5cb22912530368d
andrewmiladd <andrewmilad11@gmail.com>
2022-12-12 22:02:19 +0200

    added process.env

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -8,7 +8,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
-            secretOrKey: "secretKey",
+            secretOrKey: process.env.MY_SECRET_KEY,
         });
     }
     async validate(payload: any) {
