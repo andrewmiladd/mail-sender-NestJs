09cf9b854bf459036cbd453a0fcc3b57db29a1f9
andrewmiladd <andrewmilad11@gmail.com>
2022-12-13 15:21:25 +0200

    Protected the SendEmail route

--- src/controllers/EmailSender.controller.ts
+++ src/controllers/EmailSender.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Post, Body } from "@nestjs/common";
+import { Controller, Post, Body, UseGuards } from "@nestjs/common";
 import { EmailDto } from "src/DTO/Email.dto";
 import { EmailSendService } from "src/services/EmailSender.service";
+import { AuthGuard } from "@nestjs/passport";
 
 @Controller("sendEmail")
 export class EmailSenderController {
     constructor(private mailService: EmailSendService) {}
+    @UseGuards(AuthGuard("jwt"))
     @Post()
     async SendingEmail(@Body() toEmail: EmailDto) {
         const response = await this.mailService.singleMail(toEmail.email, toEmail.message);
