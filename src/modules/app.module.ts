7eacf511df2aee400d034f8443d01dd9f572959a
andrewmiladd <andrewmilad11@gmail.com>
2022-12-21 16:50:07 +0200

    Added EmailSender.module

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,12 +1,27 @@
+import { MailerModule } from "@nestjs-modules/mailer";
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { EmailSenderController } from "src/controllers/EmailSender.controller";
+import { EmailSendService } from "src/services/EmailSender.service";
 import { AppController } from "../controllers/app.controller";
 import { AppService } from "../services/app.service";
 import { UserModule } from "./user.module";
-import { EmailSenderModule } from "./EmailSender.module";
 
 @Module({
-    imports: [UserModule, EmailSenderModule],
-    controllers: [AppController],
-    providers: [AppService],
+    imports: [
+        ConfigModule.forRoot(),
+        UserModule,
+        MailerModule.forRoot({
+            transport: {
+                host: "smtp.gmail.com",
+                auth: {
+                    user: process.env.MY_USER,
+                    pass: process.env.MY_PASS,
+                },
+            },
+        }),
+    ],
+    controllers: [AppController, EmailSenderController],
+    providers: [AppService, EmailSendService],
 })
 export class AppModule {}
